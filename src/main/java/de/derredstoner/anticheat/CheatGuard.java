8e7cecdae776f2ad2585adf8cf72d4167e517206
BaGuAr <92256065+BaGuAr@users.noreply.github.com>
2022-07-14 20:42:46 +0900

    Update CheatGuard.java

--- src/main/java/de/derredstoner/anticheat/CheatGuard.java
+++ src/main/java/de/derredstoner/anticheat/CheatGuard.java
@@ -41,7 +41,7 @@ public class CheatGuard extends JavaPlugin {
 
         this.serverWatcher = new ServerWatcher();
 
-        this.pledge = Pledge.build().range(Short.MIN_VALUE, (short) 0);
+        this.pledge = Pledge.build().range(Short.MIN_VALUE, (short) -1);
         this.pledge.start(this);
 
         Bukkit.getPluginManager().registerEvents(new BukkitListener(), this);
